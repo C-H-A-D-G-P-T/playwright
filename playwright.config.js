be22c8032300229f71079166b76f22db46451dab
Warit <warit.automate@gmail.com>
2026-03-05 01:31:29 +0700

    fix config

--- playwright.config.js
+++ playwright.config.js
@@ -1,60 +1,56 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
-import 'dotenv/config';
-
+import { defineConfig, devices } from "@playwright/test";
+import "dotenv/config";
 
 export default defineConfig({
-  testDir: './tests',
+	testDir: "./tests",
 
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
 
-  use: {
-    baseURL: 'https://thinking-tester-contact-list.herokuapp.com',
-    trace: 'on-first-retry',
-  },
+	use: {
+		baseURL: "https://thinking-tester-contact-list.herokuapp.com",
+		trace: "on-first-retry",
+	},
 
-  projects: [
-    // ======================
-    // UI PROJECTS (Browsers)
-    // ======================
-    // {
-    //   name: 'chromium',
-    //   testDir: './tests/ui',
-    //   use: {
-    //     ...devices['Desktop Chrome'],
-    //     baseURL: 'http://localhost:3000',
-    //   },
-    // },
-    // {
-    //   name: 'firefox',
-    //   testDir: './tests/ui',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //     baseURL: 'http://localhost:3000',
-    //   },
-    // },
-    // {
-    //   name: 'webkit',
-    //   testDir: './tests/ui',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //     baseURL: 'http://localhost:3000',
-    //   },
-    // },
+	projects: [
+		// ======================
+		// UI PROJECTS (Browsers)
+		// ======================
+		{
+			name: "chromium",
+			testDir: "./tests/ui",
+			use: {
+				...devices["Desktop Chrome"],
+			},
+		},
+		// {
+		//   name: 'firefox',
+		//   testDir: './tests/ui',
+		//   use: {
+		//     ...devices['Desktop Firefox'],
+		//   },
+		// },
+		// {
+		//   name: 'webkit',
+		//   testDir: './tests/ui',
+		//   use: {
+		//     ...devices['Desktop Safari'],
+		//   },
+		// },
 
-    // ======================
-    // API PROJECT
-    // ======================
-    {
-      name: 'api',
-      testDir: './tests/api',
-      use: {
-        baseURL: 'https://thinking-tester-contact-list.herokuapp.com',
-      },
-    },
-  ],
+		// ======================
+		// API PROJECT
+		// ======================
+		{
+			name: "api",
+			testDir: "./tests/api",
+			use: {
+				baseURL: "https://thinking-tester-contact-list.herokuapp.com",
+			},
+		},
+	],
 });
