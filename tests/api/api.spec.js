3201ebeb6370a78c66f3a805776149e8a93c48d8
Warit <warit.automate@gmail.com>
2026-03-05 02:09:59 +0700

    create getToken()

--- tests/api/api.spec.js
+++ tests/api/api.spec.js
@@ -2,58 +2,63 @@ import { test, expect } from "@playwright/test";
 import { prepareContactData } from "../../utils/DataPrep.js";
 
 const dataPrep = prepareContactData();
-let token;
-
-test.beforeAll("get token first", async ({ request }) => {
-	const getToken = await request.post("/users/login", {
-		data: {
-			email: 'mycontacttest@yopmail.com',
-			password: 'Qwerty1209!',
+const getToken = async () => {
+	const response = await fetch(
+		"https://thinking-tester-contact-list.herokuapp.com/users/login",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email: "mycontacttest@yopmail.com",
+				password: "Qwerty1209!",
+			}),
 		},
-	});
-	console.log(getToken.status());
-	expect(getToken.ok()).toBeTruthy();
-	const response = await getToken.json();
-	token = response.token;
-});
+	);
+	const data = await response.json();
+	console.log(data);
+	return data.token;
+};
 
-test("should sign up successfully", async ({ request }) => {
-	const signUp = await request.post("/users", {
-		data: {
-			firstName: dataPrep.firstName,
-			lastName: dataPrep.lastName,
-			email: dataPrep.email,
-			password: dataPrep.password,
-		},
-	});
+// test("should sign up successfully", async ({ request }) => {
+// 	const signUp = await request.post("/users", {
+// 		data: {
+// 			firstName: dataPrep.firstName,
+// 			lastName: dataPrep.lastName,
+// 			email: dataPrep.email,
+// 			password: dataPrep.password,
+// 		},
+// 	});
 
-	const response = await signUp.json();
-	console.log(response);
+// 	const response = await signUp.json();
+// 	console.log(response);
 
-	expect(signUp.ok()).toBeTruthy();
-	expect(response.user.firstName).toBe(dataPrep.firstName);
-	expect(response.user.lastName).toBe(dataPrep.lastName);
-	expect(response.user.email).toBe(dataPrep.email);
-});
+// 	expect(signUp.ok()).toBeTruthy();
+// 	expect(response.user.firstName).toBe(dataPrep.firstName);
+// 	expect(response.user.lastName).toBe(dataPrep.lastName);
+// 	expect(response.user.email).toBe(dataPrep.email);
+// });
 
-test("should log in successfully", async ({ request }) => {
-	const logIn = await request.post("/users/login", {
-		data: {
-			email: 'mycontacttest@yopmail.com',
-			password: 'Qwerty1209!',
-		},
-	});
+// test("should log in successfully", async ({ request }) => {
+// 	const logIn = await request.post("/users/login", {
+// 		data: {
+// 			email: "mycontacttest@yopmail.com",
+// 			password: "Qwerty1209!",
+// 		},
+// 	});
 
-	const response = await logIn.json();
-	console.log(response);
+// 	const response = await logIn.json();
+// 	console.log(response);
 
-	expect(logIn.ok()).toBeTruthy();
-	expect(response.user.firstName).toBe('Test');
-	expect(response.user.lastName).toBe('User');
-	expect(response.user.email).toBe('mycontacttest@yopmail.com');
-});
+// 	expect(logIn.ok()).toBeTruthy();
+// 	expect(response.user.firstName).toBe("Test");
+// 	expect(response.user.lastName).toBe("User");
+// 	expect(response.user.email).toBe("mycontacttest@yopmail.com");
+// });
 
 test("should get user profile successfully", async ({ request }) => {
+	const token = await getToken();
 	const getProfile = await request.get("/users/me", {
 		headers: {
 			Authorization: `Bearer ${token}`,
@@ -64,7 +69,7 @@ test("should get user profile successfully", async ({ request }) => {
 	console.log(response);
 
 	expect(getProfile.ok).toBeTruthy();
-	expect(response.firstName).toBe('Test');
-	expect(response.lastName).toBe('User');
-	expect(response.email).toBe('mycontacttest@yopmail.com');
+	expect(response.firstName).toBe("Test");
+	expect(response.lastName).toBe("User");
+	expect(response.email).toBe("mycontacttest@yopmail.com");
 });
